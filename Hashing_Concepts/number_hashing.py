0c23f0215d377482638316879c34e50959910017
PradeepSaptasagar <pradeepsaptasagar2004@gmail.com>
2026-07-03 22:18:21 +0530

    Python advanced concepts

--- Hashing_Concepts/number_hashing.py
+++ Hashing_Concepts/number_hashing.py
@@ -1,22 +1,22 @@
 n=[5,3,2,2,1,5,5,7,5,10]
 m=[10,111,1,9,5,67,2]
-# for num in m:
-#     count=0
-#     for x in n:
-#         if x==num:
-#             count+=1
-#     print(count)
+for num in m:
+    count=0
+    for x in n:
+        if x==num:
+            count+=1
+    print(count)
 
 # Time complexity=O(m X N), Space complexity=O(1)
 
-# hash_list=[0]*11
-# for num in n:
-#     hash_list[num]+=1
-# for num in m:
-#     if num<1 or num>10:
-#         print(0)
-#     else:
-#         print(hash_list[num])
+hash_list=[0]*11
+for num in n:
+    hash_list[num]+=1
+for num in m:
+    if num<1 or num>10:
+        print(0)
+    else:
+        print(hash_list[num])
 
 # Time complexity=O(m + N), Space complexity=O(11)≈O(1)
 freq_dict={}
