4b43c97f2960c8e1501be6d1c1241417b8a5daab
PradeepSaptasagar <pradeepsaptasagar2004@gmail.com>
2026-07-12 23:03:05 +0530

    Leetcode solutions for two questions

---
+++ LeetcodeSolutions/MissinNumber.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)
+        nums.sort()
+        for i in range(0,n):
+            if nums[i]!=i:
+                return i
+        return n
